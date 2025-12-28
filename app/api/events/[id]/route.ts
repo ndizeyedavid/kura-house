2a2e2d71c3fdefcefeba29ca9ae87166b67a6024
ndizeyedavid <davidndizeye101@gmail.com>
2025-12-28 14:32:26 +0200

    deployment issue fix - id misintepratation

--- app/api/events/[id]/route.ts
+++ app/api/events/[id]/route.ts
@@ -2,12 +2,14 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 type Params = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
 export async function GET(_req: Request, { params }: Params) {
+  const resolvedParams = await params;
+
   const event = await prisma.event.findFirst({
-    where: { id: params.id, status: "PUBLISHED" },
+    where: { id: resolvedParams.id, status: "PUBLISHED" },
   });
 
   if (!event) {
